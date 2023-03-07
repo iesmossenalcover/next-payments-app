d30822cecb51bf6458a3c91b066993b45d69f867
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-03-07 13:46:03 +0100

    added create order

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+  const path = (/#!(\/.*)$/.exec(router.asPath) || [])[1];
+  if (path) {
+    router.replace(path);
+  }
+
   return <Component {...pageProps} />
 }
