3de372a9948b64152ffb9ec215708decde30b22d
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-03-08 08:28:33 +0100

    added next-routes.conf

--- deploy-scripts/start_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /var/www/paymentsapp
-rm -rf node_modules
-npm install
-pm2 start npm --name "PaymentsWeb" -- start
-pm2 save
--- deploy-scripts/stop_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo service codedeploy-agent stop
-rm -rf /opt/codedeploy-agent/deployment-root/*
-sudo service codedeploy-agent start
-pm2 stop PaymentsWeb
