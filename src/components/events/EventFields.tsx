5a9754a08fbc53fdaab473d9830e521add252ac8
Joan Barcelo Moragues <jbm@smossen.educaib>
2023-03-10 08:55:50 +0100

    datepicker not working on eventsfield.

--- src/components/events/EventFields.tsx
+++ src/components/events/EventFields.tsx
@@ -2,6 +2,7 @@ import { getGroupsSelector } from "@/lib/apis/payments/client";
 import { Event } from "@/lib/apis/payments/models";
 import { useState, useEffect } from "react";
 import { Selector, SelectorComponent } from "../Selector";
+//import DateRangePicker from "flowbite-datepicker/DateRangePicker";
 import Toggle from "../Toggle";
 
 interface EventComponentProps {
@@ -12,6 +13,11 @@ interface EventComponentProps {
 
 const EventFields = ({ event, errors, allowSetEvent = true }: EventComponentProps) => {
 
+
+//    const dateRangePickerEl = document.getElementById('dateRangePickerId');
+//    new DateRangePicker(dateRangePickerEl, {
+    // options
+//    }); 
 
     const displayErrors = (key: string) => {
         if (!errors || !errors.has(key)) return null;
@@ -26,16 +32,6 @@ const EventFields = ({ event, errors, allowSetEvent = true }: EventComponentProp
 
     return (
         <>
-            <div className="mb-6">
-                <label
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    htmlFor="code">Codi</label>
-                <input
-                    className="px-4 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 leading-tight focus:outline-none focus:bg-white"
-                    id="code" name="code" defaultValue={event.code} />
-                {displayErrors("code")}
-            </div>
-
             <div className="mb-6">
                 <label
                     className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -66,10 +62,10 @@ const EventFields = ({ event, errors, allowSetEvent = true }: EventComponentProp
                 {displayErrors("amipaPrice")}
             </div>
             
-            <div date-rangepicker className="flex items-center">
+            <div date-rangepicker="true" className="flex items-center">
                 <div className="relative">
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
                     </div>
                     <input
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -79,7 +75,7 @@ const EventFields = ({ event, errors, allowSetEvent = true }: EventComponentProp
                 <span className="mx-4 text-gray-500">to</span>
                 <div className="relative">
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
                     </div>
                     <input
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
