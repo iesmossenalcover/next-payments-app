4ab40591697fc170ea76d6215ec1ff8dbbe16bd3
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-05-25 10:29:03 +0200

    added sidebar

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -3,12 +3,12 @@ import Link from "next/link"
 
 export const SideBar = () => {
     return (
-        <nav className="min-h-screen bg-gray-900">
+        <nav className="min-h-screen bg-gray-800">
             <div className="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r hover:w-56 hover:shadow-lg">
                 <div className="flex h-screen flex-col justify-between pt-2 pb-6">
-                    <ul className="mt-6 space-y-2 tracking-wide text-white">
+                    <ul className="mt-6 space-y-2 tracking-wide text-white px-4">
                         <li className="min-w-max">
-                            <Link href="/admin" className="bg group flex items-center space-x-4 rounded-full px-4 py-3 hover:font-bold">
+                            <Link href="/admin" className="bg group flex items-center space-x-4 rounded-full py-3 hover:font-bold">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
                                 </svg>
@@ -16,7 +16,7 @@ export const SideBar = () => {
                             </Link>
                         </li>
                         <li className="min-w-max">
-                            <Link href="/" className="bg group flex items-center space-x-4 rounded-full px-4 py-3 hover:font-bold">
+                            <Link href="/" className="bg group flex items-center space-x-4 rounded-full py-3 hover:font-bold">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
                                 </svg>
@@ -25,7 +25,7 @@ export const SideBar = () => {
                             </Link>
                         </li>
                         <li className="min-w-max">
-                            <Link href="/admin/people" className="bg group flex items-center space-x-4 rounded-full px-4 py-3 hover:font-bold">
+                            <Link href="/admin/people" className="bg group flex items-center space-x-4 rounded-full py-3 hover:font-bold">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                                 </svg>
@@ -35,7 +35,7 @@ export const SideBar = () => {
                             </Link>
                         </li>
                         <li className="min-w-max">
-                            <Link href="/admin/events" className="bg group flex items-center space-x-4 rounded-full px-4 py-3 hover:font-bold">
+                            <Link href="/admin/events" className="bg group flex items-center space-x-4 rounded-full py-3 hover:font-bold">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                                 </svg>
