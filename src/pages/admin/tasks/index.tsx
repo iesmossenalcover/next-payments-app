5bea217b53a3be03ae8d12c77d6bd2d87831a4c0
Joan Barcelo Moragues <jbm@smossen.educaib>
2023-06-19 14:02:06 +0200

    button sync users with google

--- src/pages/admin/tasks/index.tsx
+++ src/pages/admin/tasks/index.tsx
@@ -2,19 +2,63 @@ import { Container } from "@/components/layout/SideBar"
 import { syncPeopleGoogleWorkspace } from "@/lib/apis/payments"
 import { useApiRequest } from "@/lib/hooks/useApiRequest";
 import { plainErrors } from "@/lib/utils";
+import React, { useState, useEffect } from 'react';
+
+
 
 const SyncPeopleToWorkspace = () => {
 
-    const { data, errors, isLoading, executeRequest } = useApiRequest(syncPeopleGoogleWorkspace);
+    const finalTime = null;
+    const [initialTime] = useState(new Date().toLocaleTimeString());
+    const [currentTime, setCurrentTime] = useState(new Date());
+  
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setCurrentTime(new Date());
+      }, 1000);
+  
+      return () => {
+        clearInterval(timer);
+      };
+    }, []);
 
-    if (errors) return <div className="text-red-500 italic">{plainErrors(errors)}</div>;
+    const { data, errors, isLoading, executeRequest: syncUsersRequest } = useApiRequest(syncPeopleGoogleWorkspace);
 
-    if (isLoading) return <>Carregant</>;
+    const updatePassword = () => {
+        const ok = confirm("Executar aquesta acció tardarà temps?");
+        if (!ok) return;
 
-    return <>
-        <button onClick={executeRequest}>Sync</button>
-        {data ? <p>{data.email}</p> : null}
-    </>
+        syncUsersRequest();
+    }
+
+    if (errors) return <div className=" mt-4 ml-4 text-red-500 italic">{plainErrors(errors)}</div>;
+
+    if (isLoading) return (<>
+        <div className="mt-4 ml-4">
+            <p>Hora Inici: {initialTime}</p>
+            <p>Hora Actual: {currentTime.toLocaleTimeString()}</p>
+            <p>Hora Fi: {finalTime === null ? "--:--:--" : finalTime}</p>
+        </div>
+    </>);
+
+    return (<>
+        <div className="mt-4 ml-4">
+            <button className='
+                        inline-block
+                        text-white 
+                        bg-blue-700 
+                        hover:bg-blue-800 
+                        focus:ring-4 
+                        focus:ring-blue-300
+                        font-medium
+                        py-3
+                        px-3
+                        rounded-lg
+                        text-sm
+                        mr-5' onClick={updatePassword}>Sync users with google G</button>
+            {data ? <p>{data.email}</p> : null}
+        </div>
+    </>)
 }
 
 export default function SyncPeopleToWorkspacePage() {
