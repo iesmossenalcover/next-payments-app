d0765d16d7e80ccd293bf4414a477f9b36309913
Sebastià Sansó <sanso.barcelo94@gmail.com>
2023-07-12 20:04:26 +0200

    fixed date

--- src/components/events/EventFields.tsx
+++ src/components/events/EventFields.tsx
@@ -61,7 +61,7 @@ const EventFields = ({ event, errors, setEvent }: EventComponentProps) => {
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Data esdeveniment"
                     name="date" id="date" type="datetime-local"
                     value={event.date}
-                    onChange={(e) => setEvent({ ...event, date: e.target.value })} />
+                    onChange={(e) => setEvent({ ...event, date: e.target.value ? e.target.value : toInputDateTime(new Date()) })} />
 
                 {displayErrors("description")}
             </div>
